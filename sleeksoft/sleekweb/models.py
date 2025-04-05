10f2c6f20fe2b0282ce0539798f368b74aa40152
Thai Pt <vuthaind@gmail.com>
2025-04-05 20:20:29 +0700

    first commit

---
+++ sleeksoft/sleekweb/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
+
+# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Quản lý tài khoản Đăng Nhập"
+    AbstractUser._meta.get_field('email').blank = False
+    AbstractUser._meta.get_field('email').blank = False
+    AbstractUser._meta.get_field('username').blank = False
+    AbstractUser._meta.get_field('username').blank = False
+    AbstractUser._meta.get_field('password').blank = False
+    AbstractUser._meta.get_field('password').blank = False
+    
+    Avatar = models.ImageField(upload_to='user_image', default="user_image/user_empty.png", null=True,blank=True)
+    Full_name = models.CharField('Họ và tên', max_length=255,blank=True, null=True)
+    Phone_number = models.CharField('Số điện thoại', max_length=255,blank=True, null=True)
+    OTP = models.CharField('Mã Otp',max_length=255, null=True,blank=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['username']),
+            models.Index(fields=['email']),
+            models.Index(fields=['Full_name']),
+        ]
+
+class Product(models.Model):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Sản phẩm"
+    
+    Avatar = models.ImageField(upload_to='PRODUCT_AVATAR',null=True,blank=True)
+    Name = models.CharField('Tên SP', max_length=500,blank=True, null=True)
+    Category = models.CharField('Danh mục', max_length=250,blank=True, null=True)
+    Price = models.CharField('Giá', max_length=255,blank=True, null=True)
+    Introduce = models.TextField('Giới thiệu',blank=True, null=True)
+    Describe = models.TextField('Mô tả',blank=True, null=True)
+    Main_ingredients = models.TextField('Thành phần chính',blank=True, null=True)
+    How_use = models.TextField('Cách sử dụng',blank=True, null=True)
+    Ingredients_table = models.TextField('Bảng thành phần',blank=True, null=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True)
+
+class Photo(models.Model):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Ảnh sản phẩm"
+    
+    Avatar = models.ImageField(upload_to='PRODUCT',null=True,blank=True)
+    Belong_Product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_photo',blank=True, null=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True) 
+
+class Photo_ncls(models.Model):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Ảnh sản phẩm NCLS"
+    
+    Avatar = models.ImageField(upload_to='PRODUCT_NCLS',null=True,blank=True)
+    Belong_Product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_photo_ncls',blank=True, null=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True) 
+
+class Size(models.Model):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Size sản phẩm"
+    
+    Name = models.CharField('Tên Size SP', max_length=500,blank=True, null=True)
+    Belong_Product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_size',blank=True, null=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True)
+
+class Email(models.Model):
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Size sản phẩm"
+    
+    Name = models.CharField('Tên email', max_length=200,blank=True, null=True)
+    Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+    Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True)
+
+# class Banner(models.Model):
+#     class Meta:
+#         ordering = ["id"]
+#         verbose_name_plural = "Ảnh Banner"
+    
+#     Photo = models.ImageField(upload_to='website/banner',null=True,blank=True)
+#     Belong_Website = models.ForeignKey(Website, on_delete=models.CASCADE, related_name='list_photo_banner',blank=True, null=True)
+#     Creation_time = models.DateTimeField('Thời gian tạo',auto_now_add=True)
+#     Update_time = models.DateTimeField('Thời gian cập nhật',auto_now=True)
+    
+
+
---
+++ sleeksoft/sleekweb/urls.py
@@ -0,0 +1,115 @@
+"""
+URL configuration for luanvan project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.contrib import admin
+# from Data_Interaction.admin import admin_site
+from django.urls import path
+
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.urls import re_path,path
+
+
+from django.views.generic.base import TemplateView
+from django.conf.urls.static import serve
+
+from django.views.generic import RedirectView
+
+from django.contrib.auth import views as auth_views
+
+from .views.client.login_client import *
+
+from .views.client.home_client import *
+from .views.client.tab_best_sellers_client import *
+from .views.client.tab_products_client import *
+from .views.client.tab_regimens_client import *
+
+from .views.client.nav_blog_client import *
+from .views.client.nav_nghien_cuu_lam_san import *
+from .views.client.nav_products_client import *
+
+from .views.client.select_kvmb_client import *
+from .views.client.select_kvmn_client import *
+
+
+from .views.client.blog_1_client import *
+from .views.client.blog_2_client import *
+from .views.client.blog_3_client import *
+
+from .views.client.about_ve_chung_toi_client import *
+from .views.client.about_tin_tuc_giai_thuong_client import *
+from .views.client.about_tam_nhin_va_su_menh_client import *
+from .views.client.about_lien_he_client import *
+
+from .views.client.link_ket_qua_lam_san_client import *
+from .views.client.link_nghien_cuu_khoa_hoc_client import *
+from .views.client.detail_product_client import *
+from .views.client.signup_email_client import *
+
+from .views.admin.login_admin import *
+from .views.admin.product_admin import *
+
+urlpatterns = [
+
+    path('account/login', login_client,name='login_client'),
+
+    path('', tab_best_sellers_client,name='tab_best_sellers_client'),
+    path('products-home', tab_products_client,name='tab_products_client'),
+    path('regimens-home', tab_regimens_client,name='tab_regimens_client'),
+    
+
+    path('blog', nav_blog_client,name='nav_blog_client'),
+    path('mineral-research', nav_nghien_cuu_lam_san,name='nav_nghien_cuu_lam_san'),
+    path('products', nav_products_client,name='nav_products_client'),
+
+    path('north-partner', select_kvmb_client,name='select_kvmb_client'),
+    path('southern-partner', select_kvmn_client,name='select_kvmn_client'),
+
+    path('blog-1', blog_1_client,name='blog_1_client'),
+    path('blog-2', blog_2_client,name='blog_2_client'),
+    path('blog-3', blog_3_client,name='blog_3_client'),
+
+    path('about-us', about_ve_chung_toi_client,name='about_ve_chung_toi_client'),
+    path('news-awards', about_tin_tuc_giai_thuong_client,name='about_tin_tuc_giai_thuong_client'),
+    path('vision-mission', about_tam_nhin_va_su_menh_client,name='about_tam_nhin_va_su_menh_client'),
+    path('contact', about_lien_he_client,name='about_lien_he_client'),
+
+    path('contact', about_lien_he_client,name='about_lien_he_client'),
+
+    path('scientific-research', link_nghien_cuu_khoa_hoc_client,name='link_nghien_cuu_khoa_hoc_client'),
+    path('clinical-results', link_ket_qua_lam_san_client,name='link_ket_qua_lam_san_client'),
+
+    path('detail-proudct/<int:pk>/', detail_product_client,name='detail_product_client'),
+
+    path('admin/login', login_admin,name='login_admin'),
+    path('admin/logout', logout_admin,name='logout_admin'),
+    path('admin/product', product_admin,name='product_admin'),
+    path('admin/product/add', product_add_admin,name='product_add_admin'),
+    path('admin/product/edit/<int:pk>/', product_edit_admin,name='product_edit_admin'),
+    path('admin/product/remove', product_remove_admin,name='product_remove_admin'),
+
+    path('admin/product/size/add', size_product_add_admin,name='size_product_add_admin'),
+    path('admin/product/size/remove',size_product_remove_admin,name='size_product_remove_admin'),
+
+    path('signup-email',signup_email_client,name='signup_email_client'),
+]
